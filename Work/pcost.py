16de39504a8c3340b94b9d25ea9d1fda34d7e040
Gerardo Abraham <gabraham@newclassrooms.org>
2020-07-01 17:18:38 -0300

    Add Exercise 1.27: Reading a data file.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+total = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        split_line = line.split(',')
+        total += int(split_line[1]) * float(split_line[2])
+
+print(f'Total cost {round(total, 2)}')
