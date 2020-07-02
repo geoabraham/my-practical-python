58eb669251efa242a9310a823b923c0df91ceb5b
Gerardo Abraham <gabraham@newclassrooms.org>
2020-07-02 12:53:30 -0300

    Add Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
-total = 0.0
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    for line in f:
-        split_line = line.split(',')
-        total += int(split_line[1]) * float(split_line[2])
 
-print(f'Total cost {round(total, 2)}')
+def portfolio_cost(filename):
+    total = 0.0
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        for line in f:
+            split_line = line.split(',')
+            total += int(split_line[1]) * float(split_line[2])
+    return total
+
+
+total_cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost {round(total_cost, 2)}')
